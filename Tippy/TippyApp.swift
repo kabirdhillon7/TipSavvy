40b6b09d531053754d685de6c88f279124572b64
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-05-14 19:13:45 -0700

    Initial Commit

---
+++ Tippy/TippyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TippyApp.swift
+//  Tippy
+//
+//  Created by Kabir Dhillon on 5/14/23.
+//
+
+import SwiftUI
+
+@main
+struct TippyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
