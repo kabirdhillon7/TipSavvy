2a11b014c5ee071f4d36a942dd8e4a83ff95ac01
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-05-24 20:47:15 -0700

    ContentView
    Updated app name

--- Tippy/ContentView.swift
+++ Tippy/ContentView.swift
@@ -66,7 +66,7 @@ struct ContentView: View {
                     Text("Bill Totals")
                 }
             }
-            .navigationTitle("Tippy")
+            .navigationTitle("TipSavvy")
         }
         .onAppear {
             // Load user defaults
