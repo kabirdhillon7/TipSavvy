e9cc71140857154745020d41217d29a130ab3640
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-10-10 22:21:39 -0700

    UI Tests

--- TippyUITests/TippyUITests.swift
+++ TippyUITests/TippyUITests.swift
@@ -8,7 +8,18 @@
 import XCTest
 
 final class TippyUITests: XCTestCase {
-
+    
+    override func setUp() {
+        super.setUp()
+        
+        let app = XCUIApplication()
+        app.launchArguments += ["-AppleLanguages", "(en-US)"]
+        app.launchArguments += ["-AppleLocale", "\"en-US\""]
+        
+        app.launch()
+        
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
